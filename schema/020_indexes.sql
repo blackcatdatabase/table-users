6d6d4e0bf47f594db07b745c27b672b55b7ddc08
Tolben <vitek.pasek@gmail.com>
2025-10-22 21:00:57 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/020_indexes.sql
+++ schema/020_indexes.sql
@@ -1,3 +1,3 @@
--- Auto-generated from schema-map.psd1 (map@1e83bb6)
+-- Auto-generated from schema-map.psd1 (map@6cefe8e)
 -- table: users
 CREATE UNIQUE INDEX ux_users_email_hash ON users (email_hash);
